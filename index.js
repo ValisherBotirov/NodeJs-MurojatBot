0b14734cfb7eb6be5e2972f78ac176781fc50756
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-06-05 18:53:08 +0500

    add reply message

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // const TELEGRAM_BOT_TOKEN = "6140449523:AAFtb3y1PR1WnihdP6_c6ExWUBTLlhxWhVk";
 
-const { log } = require("console");
 const fs = require("fs");
 
 const TeleBot = require("telebot");
@@ -14,66 +13,92 @@ const founderId = "1423191247";
 
 bot.on(["/start"], (msg) => {
   console.log(msg);
-  clientId = msg.chat.id;
   msg.reply.text(
-    `Assalomu alaykum @${msg.from.username}. Valisher Botirovning murojat botiga xush kelibsiz!`
+    `<b>Assalomu alaykum @${msg.from.username}.</b> \n\n<a href="htpps://t.me/Valisher_Botirov"><b>Valisher Botirovning</b></a> murojat botiga xush kelibsiz!\n\nXabaringizni yozib qoldiring va tez orada sizga javob qaytaramiz!\n\n<b>Ko'proq malumot uchun:</b> \n\n🔔 @Valisher_Botirov
+    `,
+    { parseMode: "html" }
   );
-
-  const allData = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-  console.log(allData, "all user");
+  const allUsers = JSON.parse(fs.readFileSync("users.json", "utf-8"));
 
   const user = {
     userId: msg.chat.id,
-    messageId: msg.message_id,
     userName: msg.chat.username,
-    user: msg.chat.first_name,
+    user: msg.chat.first_name + " " + msg.chat.last_name,
   };
 
-  allData.push(user);
+  const checkUser = allUsers.findIndex((el) => {
+    return el.userId == user.userId;
+  });
 
-  const jsonData = JSON.stringify(allData, null, 2); // Ob'ektni JSON ko'rinishiga o'girish
-  fs.writeFileSync("data.json", jsonData); // JSON faylga yozish
+  if (checkUser == -1) {
+    allUsers.push(user);
+    const jsonData = JSON.stringify(allUsers, null, 2);
+    fs.writeFileSync("users.json", jsonData);
+  } else console.log("bunday user avvaldan mavjud");
 });
 
 bot.on("text", (msg) => {
-  console.log(msg.chat.id);
-
-  if (msg.chat.id == founderId) {
-    adminPanel(msg);
-  } else {
-    userPanel(msg);
+  if (msg.text != "/start") {
+    if (msg.chat.id == founderId) {
+      adminPanel(msg);
+    } else {
+      userPanel(msg);
+    }
   }
 });
 
 function adminPanel(msg) {
   console.log(msg);
 
-  const users = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-  // console.log(msg.reply_to_message.message_id);
+  const replyToMessage = msg.reply_to_message;
+  const replyToMessageId = replyToMessage ? replyToMessage.message_id : null;
+  console.log(replyToMessageId, "repLyId");
 
-  users.forEach((user) => {
+  if (replyToMessageId) {
+    let userId = msg.reply_to_message.text.slice(4, 15);
+    console.log(userId);
+
     bot
-      .sendMessage(user.userId, msg.text)
+      .sendMessage(userId, msg.text, {
+        reply_to_message_id: userId.messageId,
+      })
       .then((res) => {
         console.log(res);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
-  // bot.sendMessage(
-  //   5844994207,
-  //   "Server bilan muommo yuz berdi. Eror | status code 500"
-  // );
+  }
+
+  // hamma userga birder xabar jo'natish uchun cod
+
+  // users.forEach((user) => {
+  //   bot
+  //     .sendMessage(user.userId, msg.text)
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // });
 }
 
 function userPanel(msg) {
-  console.log("run is users");
   console.log(msg);
-  let text = `Sizga username @${msg.from.username}, ismi ${msg.chat.first_name} bo'lgan foydalanuvchi xabar yo'lladi.  \n \n \n ${msg.text}`;
-  bot.sendMessage(founderId, text);
+  let text = `<b>Id:</b> ${msg.chat.id}\n<b>MessageId:</b> ${
+    msg.message_id
+  }\n<b>Username:</b> @${msg.from.username}\n<b>Ismi:</b> ${
+    msg.chat.first_name
+  }  ${
+    msg.chat.last_name ? msg.chat.last_name : ""
+  } \n\n <i>Yuborgan xabari:</i>\n\n ${msg.text}`;
+  bot.sendMessage(founderId, text, { parseMode: "html" });
 
-  msg.reply.text("So'rovingiz uchun rahmat! Tez orada siz bilan bog'lanamiz");
+  msg.reply.text(
+    "So'rovingiz uchun rahmat! Tez orada siz bilan bog'lanamiz\n\n🎤 <b>MurojatBot</b>",
+    { parseMode: "html" }
+  );
 }
 
 bot.start();
